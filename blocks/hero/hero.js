0661ec4a2d28c2ca399c4387cb01a8e0f889f1e6
Palani <npalani@deloitte.com>
2024-12-06 17:18:12 +0530

    syntax issues resolved

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,12 +1,9 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
-
 export default function decorate(block) {
-  console.log(block);
-
   const [heroWrapper] = block.children;
   const blockquote = document.createElement('hero-banner');
   blockquote.textContent = heroWrapper.textContent.trim();
   heroWrapper.replaceChildren(blockquote);
-};
+}
