e625bc14faeb4a087a3df8b06e77a21d2eae9d99
Palani <npalani@deloitte.com>
2024-12-12 14:39:17 +0530

    removed code from hero

--- blocks/hero/hero.js
+++
@@ -1,9 +0,0 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
-import { moveInstrumentation } from '../../scripts/scripts.js';
-
-export default function decorate(block) {
-  const [heroWrapper] = block.children;
-  const blockquote = document.createElement('hero-banner');
-  blockquote.textContent = heroWrapper.textContent.trim();
-  heroWrapper.replaceChildren(blockquote);
-}
