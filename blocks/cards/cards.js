4c16acc70c8998f75036672a85a7260e8bd5c5df
Palani <npalani@deloitte.com>
2024-12-12 13:11:41 +0530

    removed spaces

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -19,7 +19,6 @@ export default function decorate(block) {
     });
     ul.append(li);
   });
-  
   ul.querySelectorAll('picture > img').forEach((img) => {
     const optimizedPic = createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]);
     moveInstrumentation(img, optimizedPic.querySelector('img'));
